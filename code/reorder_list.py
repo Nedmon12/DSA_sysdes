6c4779e9fbcbfab2dec8d4b33a9ec5acd3701bd9
Nedmon12 <mentesnot.ertibu@gmail.com>
2024-01-31 13:09:30 +0300

    override reorder_list.py

--- code/reorder_list.py
+++ code/reorder_list.py
@@ -1,22 +1,45 @@
-# Definition of singly-linked list
-# class ListNode:
-#   def __init__(self, val=0, next=None):
-#       self.val = val
-#       self.next = next
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+       
+        prev = None
         while head:
-            #iterator = head
-            ittwin = iterator = ListNode()
-            ittwin = iterator = head
-            while iterator:
-                if not iterator.next:
-                    # number is to be neighbor to head
-                    temp = head
-                    head.next = iterator
-                    iterator.next = temp.next
-                    head = temp.next
-                    ittwin.next = None
-                ittwin = iterator
-                iterator = iterator.next
-        head = head.next
+           temp = head.next
+           head.next = prev
+           prev = head
+           head = temp
+        tail = None
+        prevy = prev
+        count = 1
+        if count == n:
+            heady = None
+            prevy = prevy.next
+            while prevy:
+                temp = prevy.next
+                prevy.next = heady
+                heady = prevy
+                prevy = temp
+            return heady
+        while count <= n and prev:
+            if count == n:
+                #if count == 1:
+                    #prev = prev.next
+                    #break
+                if prev.next is None:
+                    if tail is not None:
+                        tail.next = None
+                        break
+                else:
+                    tail.next = prev.next
+                    break
+            count +=1
+            tail = prev
+            prev = prev.next
+        head = None
+        while prevy:
+           temp = prevy.next
+           prevy.next = head
+           head = prevy
+           prevy = temp
+        return head
+
+
