36d38655031e5d7efc35a4c21ae3fae47b10fd69
Nedmon12 <mentesnot.ertibu@gmail.com>
2024-01-31 11:35:53 +0300

    override previous commit

---
+++ code/remove_Nth_node.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+       
+        def delnode():
+            # define delete node here
+            # yes delnode is god
+
+        #find node and send to god
